268607b30cf62ea33eae21fff72f582f32d67461
JMQ0598 <32473670+JMQ0598@users.noreply.github.com>
2026-01-15 23:49:50 -0800

    change root node to rigidbody2d, add boulder

---
+++ pogo-game/Assets/Scripts/boulder.gd
@@ -0,0 +1,7 @@
+extends RigidBody2D
+
+@export var gravity : float = 10
+@export var rotation_speed : float = 5
+
+func _physics_process(_delta):
+	pass
--- pogo-game/Assets/Scripts/camera_follow.gd
+++ pogo-game/Assets/Scripts/camera_follow.gd
@@ -3,10 +3,8 @@ extends Camera2D
 # Please assign a target in the editor
 @export var follow_target : Node2D = null
 
-func _ready():
-	pass
-
 func _physics_process(_delta):
 	
 	# Set to match position of target
-	global_position = follow_target.global_position
+	if (follow_target.global_position.y < global_position.y):
+		global_position.y = follow_target.global_position.y
