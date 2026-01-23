07c3cf13491d4219bbf6a9630f404bc51063a512
JMQ0598 <32473670+JMQ0598@users.noreply.github.com>
2026-01-23 00:26:10 -0800

    center jump demo

--- pogo-game/Assets/Scripts/camera_follow.gd
+++ pogo-game/Assets/Scripts/camera_follow.gd
@@ -2,9 +2,10 @@ extends Camera2D
 
 # Please assign a target in the editor
 @export var follow_target : Node2D = null
+@export var can_go_down : bool = false
 
 func _physics_process(_delta):
 	
 	# Set to match position of target
-	if (follow_target.global_position.y < global_position.y):
+	if (!can_go_down and follow_target.global_position.y < global_position.y):
 		global_position.y = follow_target.global_position.y
