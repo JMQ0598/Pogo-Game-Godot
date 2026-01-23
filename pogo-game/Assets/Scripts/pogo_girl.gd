2ee8f591e84db3a2633ab7b0409dbeb3d3a967c8
kyungminkim1 <kyungminkimnz@gmail.com>
2026-01-23 21:19:23 +1300

    Set JumpDemo as MainScene

--- pogo-game/Assets/Scripts/pogo_girl.gd
+++ pogo-game/Assets/Scripts/pogo_girl.gd
@@ -5,7 +5,7 @@ var isMidair: bool = true
 @export var speed: int = 1000
 @export var jump_speed = -1800
 @export var gravity: int = 4000
-@export var bounce: int = 4000
+@export var bounce: int = -90000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,14 +17,18 @@ func _process(delta: float) -> void:
 	pass
 		
 func _physics_process(delta: float) -> void:
-	velocity.y = gravity * delta
-	if isMidair:
+	
+	if is_on_floor():
+		velocity.y = 0
+		await get_tree().create_timer(1.0).timeout
+		velocity.y = bounce * delta
+	else:
+		velocity.y = gravity * delta
 		var dir = Input.get_axis('walk_left','walk_right')
 		if dir != 0:
 			velocity.x = dir * speed
 		else:
 			velocity.x = 0
-	else:
-		if is_on_floor():
-			velocity.y = bounce * delta
+		
 	move_and_slide()
+	
