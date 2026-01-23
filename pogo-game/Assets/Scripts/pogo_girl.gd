4092d80a19f66bcc25c0c332d6e1ba0712fd5bb7
kyungminkim1 <kyungminkimnz@gmail.com>
2026-01-23 20:30:26 +1300

    Update WIP jump behaviour

--- pogo-game/Assets/Scripts/pogo_girl.gd
+++ pogo-game/Assets/Scripts/pogo_girl.gd
@@ -1,17 +1,11 @@
 extends CharacterBody2D
 
-
-
 var isMidair: bool = true
 @export var health: int = 3
 @export var speed: int = 1000
 @export var jump_speed = -1800
 @export var gravity: int = 4000
-@export var bounce: int = 200
-@export var jump: int = 400
-@export_range(0.0, 1.0) var friction: float = 0.1
-@export_range(0.0, 1.0) var acceleration: float = 0.25
-
+@export var bounce: int = 4000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -27,9 +21,10 @@ func _physics_process(delta: float) -> void:
 	if isMidair:
 		var dir = Input.get_axis('walk_left','walk_right')
 		if dir != 0:
-			velocity.x = lerp(velocity.x, dir * speed, acceleration)
+			velocity.x = dir * speed
 		else:
-			velocity.x = lerp(velocity.x, 0.0, friction)
+			velocity.x = 0
 	else:
-		velocity.y = bounce * delta
+		if is_on_floor():
+			velocity.y = bounce * delta
 	move_and_slide()
