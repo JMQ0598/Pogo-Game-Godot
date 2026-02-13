f5f0c9805ee049c884776c934bd1fe6f8e6d04d7
kyungminkim1 <kyungminkimnz@gmail.com>
2026-02-13 19:45:19 +1300

    Push to avoid merge conflict

--- pogo-game/Assets/Scenes/platform_manager.gd
+++ pogo-game/Assets/Scenes/platform_manager.gd
@@ -3,7 +3,9 @@ extends Node2D
 var platform_prefab = preload("res://Assets/Scenes/platform.tscn")
 @export var follow_target : Node2D = null
 @export var space_required: int = 400
-var next_platform_level: int = -600
+@export var next_platform_level: int = -600
+@export var next_platform_space = 440
+
 
 func _ready() -> void:
 	pass
@@ -13,7 +15,7 @@ func _process(delta: float) -> void:
 		var platform_copy = platform_prefab.instantiate()
 		platform_copy.global_position = Vector2(0, next_platform_level)
 		add_child(platform_copy)
-		next_platform_level -= 440
-	
+		next_platform_level -= next_platform_space
+	 
 	
 	
