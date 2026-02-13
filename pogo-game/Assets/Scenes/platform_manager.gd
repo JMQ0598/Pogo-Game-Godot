c7d41bb49a2c67f363203fc25673ebb2c8d65207
kyungminkim1 <kyungminkimnz@gmail.com>
2026-02-13 21:06:37 +1300

    Add necessary condition before deleting platforms

--- pogo-game/Assets/Scenes/platform_manager.gd
+++ pogo-game/Assets/Scenes/platform_manager.gd
@@ -3,7 +3,7 @@ extends Node2D
 var platform_prefab = preload("res://Assets/Scenes/platform.tscn")
 @export var follow_target : Node2D = null
 @export var space_required: int = 400
-@export var next_platform_level: int = 500
+@export var next_platform_level: int = 300
 @export var platform_spacing: int = 440
 @export var bottom_boundary: int = 0
 @export var platforms_queue = []
@@ -19,9 +19,8 @@ func _process(delta: float) -> void:
 		next_platform_level -= platform_spacing
 		platforms_queue.push_back(platform_copy)
 		
-	if follow_target.global_position.y - next_platform_level > bottom_boundary:
-		platforms_queue.pop_front()
-		
-	 
-	
-	
+	if follow_target.global_position.y + next_platform_level > bottom_boundary:
+		if len(platforms_queue) > 0:
+			var oldest_platform = platforms_queue.pop_front()
+			oldest_platform.queue_free()
+			print('deleted')
