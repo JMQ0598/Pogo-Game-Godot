3e3dd4be87ecb6bca3456c043a2ea36a1f6bd895
kyungminkim1 <kyungminkimnz@gmail.com>
2026-02-20 20:26:53 +1300

    Working on platformmanager

--- pogo-game/Assets/Scenes/platform_manager.gd
+++ pogo-game/Assets/Scenes/platform_manager.gd
@@ -2,25 +2,26 @@ extends Node2D
 
 var platform_prefab = preload("res://Assets/Scenes/platform.tscn")
 @export var follow_target : Node2D = null
+@export var scene_camera : Camera2D = null
 @export var space_required: int = 400
 @export var next_platform_level: int = 300
-@export var platform_spacing: int = 440
-@export var bottom_boundary: int = 0
+@export var platform_spacing: int = 200
+@export var bottom_boundary: int = 100
 @export var platforms_queue = []
 
 func _ready() -> void:
 	pass
 	
 func _process(delta: float) -> void:
-	if follow_target.global_position.y - next_platform_level < space_required:
+	if position.y - scene_camera.position.y <= space_required:
 		var platform_copy = platform_prefab.instantiate()
 		platform_copy.global_position = Vector2(0, next_platform_level)
 		add_child(platform_copy)
 		next_platform_level -= platform_spacing
 		platforms_queue.push_back(platform_copy)
 		
-	if follow_target.global_position.y + next_platform_level > bottom_boundary:
+	if follow_target.global_position.y + next_platform_level < bottom_boundary :
 		if len(platforms_queue) > 0:
 			var oldest_platform = platforms_queue.pop_front()
 			oldest_platform.queue_free()
-			print('deleted')
+			bottom_boundary -= platform_spacing
