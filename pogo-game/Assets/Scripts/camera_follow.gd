9294f79f8aca85eeca46bbd89e6269f2bf5135a1
JMQ0598 <32473670+JMQ0598@users.noreply.github.com>
2026-02-19 23:24:32 -0800

    slight camera tweaks, still needs adjusting so it moves up every platform jump (150 px)

--- pogo-game/Assets/Scripts/camera_follow.gd
+++ pogo-game/Assets/Scripts/camera_follow.gd
@@ -2,28 +2,35 @@ extends Camera2D
 
 # Please assign a target in the editor
 @export var follow_target : Node2D = null
-@export var can_go_down : bool
-@export var camera_speed : float = 400
-var focus_height : float = 0
-var focus_height_offset : float = 300
+@export var free_follow_cam : bool
+@export var camera_speed : float = 500
+
+# This variable indicates where the camera "should" be in order to follow
+#	the player properly. -150 is its initial value because 150 is the distance
+#	between platforms.
+var focus_height : float = -150
 
 func _ready():
 	focus_height = follow_target.global_position.y
 
 func _physics_process(delta):
 	
-	# focus_height checkpoint
-	var camera_target = follow_target.global_position.y + focus_height_offset
+	# The camera target's the player's y position
+	var camera_target = follow_target.global_position.y
 	if (camera_target < focus_height):
 		focus_height = camera_target
 	
 	# DEBUG ONLY
-	if (can_go_down):
-		global_position.y = follow_target.global_position.y
+	if (free_follow_cam):
+		global_position.y = camera_target
 
-	# In-game behavior
+	# If the camera's actual height is below where it's supposed to be...
 	elif (focus_height < global_position.y):
+		
+		# Move towards that location.
 		global_position.y -= camera_speed * delta
+		
+		# Do not let the camera's actual height go above the focus height.
 		if (focus_height > global_position.y):
 			global_position.y = focus_height
 			
