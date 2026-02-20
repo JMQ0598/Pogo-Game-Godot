0fa9493dfcadc9fd835c1f64e3327b94869b8cb8
kyungminkim1 <kyungminkimnz@gmail.com>
2026-02-20 19:26:01 +1300

    Scale down pogo girl

--- pogo-game/Assets/Scripts/pogo_girl.gd
+++ pogo-game/Assets/Scripts/pogo_girl.gd
@@ -11,7 +11,6 @@ extends CharacterBody2D
 func _ready() -> void:
 	$AnimatedSprite2D.play('fall')
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
