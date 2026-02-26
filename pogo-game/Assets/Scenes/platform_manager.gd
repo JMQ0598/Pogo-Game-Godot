629298260ac51e623030703e8b29efcfe3e06c15
kyungminkim1 <kyungminkimnz@gmail.com>
2026-02-26 20:36:04 +1300

    Rewriting PlatformManager script WIP

--- pogo-game/Assets/Scenes/platform_manager.gd
+++ pogo-game/Assets/Scenes/platform_manager.gd
@@ -1,27 +1,27 @@
 extends Node2D
 
 var platform_prefab = preload("res://Assets/Scenes/platform.tscn")
-@export var follow_target : Node2D = null
+@export var player : Node2D = null
 @export var scene_camera : Camera2D = null
-@export var space_required: int = 400
-@export var next_platform_level: int = 300
 @export var platform_spacing: int = 200
-@export var bottom_boundary: int = 100
 @export var platforms_queue = []
 
+var camera_vertical_bound: int
+@export var bounds_buffer: int = 100
+
 func _ready() -> void:
-	pass
+	camera_vertical_bound = DisplayServer.screen_get_size().y / 2 + bounds_buffer
+	position.y = scene_camera.position.y + camera_vertical_bound
 	
 func _process(delta: float) -> void:
-	if position.y - scene_camera.position.y <= space_required:
+	if absi(scene_camera.position.y - camera_vertical_bound) <= camera_vertical_bound:
 		var platform_copy = platform_prefab.instantiate()
-		platform_copy.global_position = Vector2(0, next_platform_level)
+		platform_copy.global_position = Vector2(0, position.y)
 		add_child(platform_copy)
-		next_platform_level -= platform_spacing
 		platforms_queue.push_back(platform_copy)
 		
-	if follow_target.global_position.y + next_platform_level < bottom_boundary :
-		if len(platforms_queue) > 0:
-			var oldest_platform = platforms_queue.pop_front()
-			oldest_platform.queue_free()
-			bottom_boundary -= platform_spacing
+	#if follow_target.global_position.y + next_platform_level < bottom_boundary :
+		#if len(platforms_queue) > 0:
+			#var oldest_platform = platforms_queue.pop_front()
+			#oldest_platform.queue_free()
+			#bottom_boundary -= platform_spacing
